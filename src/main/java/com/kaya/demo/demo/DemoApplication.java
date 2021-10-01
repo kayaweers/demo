ad8801cf13153e6f47b37502a2885b65446e0ff2
kayaweers <kaya@weers.net>
2021-10-01 14:38:22 +0200

    CT: commit from code participant

--- src/main/java/com/kaya/demo/demo/DemoApplication.java
+++ src/main/java/com/kaya/demo/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+    // comment participant CT
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);

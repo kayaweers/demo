2b585aaa5f11a5ee0c39b7c7d804d9b20af8651c
kayaweers <kaya@weers.net>
2024-02-02 10:23:36 +0100

    clean up repo

--- src/main/java/com/kaya/demo/demo/DemoApplication.java
+++ src/main/java/com/kaya/demo/demo/DemoApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-    // comment participant CT
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
